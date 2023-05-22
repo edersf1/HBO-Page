ea3fe6d2894c894ef7f03f8ad5210336a8e4f367
Eder Santos <edersf1999@gmail.com>
2023-05-22 12:42:07 -0300

    first commit

---
+++ scrypts.js
@@ -0,0 +1,27 @@
+//Seleciona o elemento de video:
+const video = document.getElementById("video-player");
+
+//Definindo o array com as URLs dos videos:
+const videosource = [
+    "https://cmaf.fly.latam.hbomaxcdn.com/videos/GYuFxFgzSGcIMvgEAAAAG/v/0_3d5bc3/v12.mp4",
+    "https://cmaf.akm.latam.hbomaxcdn.com/videos/GYk7rQgnSAJ25wgEAAAAC/0/cda460/v/v13.mp4",
+    "https://cmaf.cf.latam.hbomaxcdn.com/videos/GYZ1KOABV3oydnQEAAAAE/0/f83e29/v/v11.mp4"
+];
+
+//Definir o indice inicial
+let currentVideoIndex = 0;
+
+video.addEventListener("ended", () => {
+
+    
+    if (currentVideoIndex === videosource.length - 1) {
+        currentVideoIndex = 0;
+    }   else {
+        currentVideoIndex++;
+    }
+    
+    video.src = videosource[currentVideoIndex];
+    
+    
+    video.play();
+})
